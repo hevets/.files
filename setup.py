191e3208a3dbf3d1c023c27070e0482452d97ab6
Steve Henderson <hevets@gmail.com>
2014-04-27 13:36:31 -0700

    simplifying config

--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 import os
 
+# clean screen prep install
 os.system('clear')
 
 # remove .vim settings
@@ -11,27 +12,18 @@ os.system("rm ~/.ackrc")
 os.system("rm ~/.gitconfig")
 os.system("rm ~/.jshintrc")
 os.system("rm ~/.vimrc")
-os.system("rm ~/.zshrc")
-os.system("rm ~/.tmux.conf")
+os.system("rm ~/.bashrc")
+# os.system("rm ~/.tmux.conf") re-learn vim, then tmux
 
 # create links to config files
 os.system("ln -s ~/.files/.ackrc ~/.ackrc")
 os.system("ln -s ~/.files/.gitconfig ~/.gitconfig")
 os.system("ln -s ~/.files/.jshintrc ~/.jshintrc")
 os.system("ln -s ~/.files/.vimrc ~/.vimrc")
-os.system("ln -s ~/.files/.zshrc ~/.zshrc")
-os.system("ln -s ~/.files/.tmux.conf ~/.tmux.conf")
+os.system("ln -s ~/.files/.bashrc ~/.bashrc")
+# os.system("ln -s ~/.files/.tmux.conf ~/.tmux.conf") re-learn vim, then tmux
 
 # clone in vundle & zsh
 os.system("git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle")
 os.system('git submodule update --init')
 os.system('vim +BundleInstall +qall')
-
-
-# I like to use Command + Shift + N (new session)
-# and Command + Shift + T (resume target session)
-print "install tmux with homebrew"
-print "tmux bindings"
-print "Add to iterm2 key bindings"
-print "tmux new -s hevets"
-print "tmux a -t hevets"
